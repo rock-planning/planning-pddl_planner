c29870fb05cb06686557a32068204210f77ca09f
Thomas Roehr <thomas.roehr@dfki.de>
2012-07-02 13:54:32 +0200

    Add console interface for pddl-planner

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,19 +1,100 @@
 #include <iostream>
 #include <stdio.h>
+#include <map>
+#include <errno.h>
 #include <pddl_planner/PDDLPlanner.hpp>
 #include <pddl_planner/planners/Lama.hpp>
+
+void usage(int argc, char** argv)
+{
+    printf("usage: %s [-p <planner-name>] <domain-description-file> <problem-file>\n",argv[0]);
+}
 
 int main(int argc, char** argv)
 {
     using namespace pddl_planner;
-    lama::Planner planner;
-    PDDLPlanner pddlPlanner(&planner);
 
-    pddlPlanner.setDomainDescription("rimres", "(define (domain rimres)\n(:requirements :strips :equality :typing :conditional-effects)\n(:types location physob_id physob_type)\n(:constants sherpa crex payload - physob_type)\n(:predicates ( at ?x - physob_id ?l - location)\n( is_a ?x - physob_id ?r - physob_type)\n( connected ?x ?y - physob_id)\n( cannot_move ?x - physob_id)\n)\n\n(:action move\n :parameters (?obj - physob_id ?m ?l - location)\n:precondition ( and (at ?obj ?m) (not (= ?m ?l)) (not (cannot_move ?obj) ))\n :effect (and (at ?obj ?l) (not (at ?obj ?m))\n (forall (?z)\n (when (and (connected ?z ?obj) (not (= ?z ?obj)))\n (and (at ?z ?l) (not (at ?z ?m)))\n)))\n)\n (:action move_into_range\n :parameters (?x ?y - physob_id ?m ?l - location)\n :precondition (and (not (cannot_move ?x)) (at ?x ?m) (at ?y ?l) )\n :effect (and (at ?x ?l) (at ?y ?l) (not (at ?x ?m)))\n)\n (:action connect\n :parameters (?x ?y - physob_id ?l - location)\n :precondition (and (at ?x ?l) (at ?y ?l))\n :effect (and (connected ?x ?y) (cannot_move ?y))\n)\n(:action disconnect\n :parameters (?x ?y - physob_id)\n :precondition (and (not (= ?x ?y)) (connected ?x ?y)) \n :effect (and (not (connected ?x ?y)) (not (cannot_move ?y)))\n)\n)\n");
+    std::map<std::string, PDDLPlannerInterface*> knownPlanners;
+    lama::Planner lamaPlanner;
+    knownPlanners[lamaPlanner.getName()] = &lamaPlanner;
+
+    PDDLPlannerInterface* planner = NULL;
+
+    // defaultPlanner is LAMA
+    if(argc < 2 )
+    {
+        usage(argc, argv);
+        exit(0);
+    }
+
+    std::string firstArg = argv[1];
+    if(firstArg == "-h" || firstArg == "--help")
+    {
+        usage(argc, argv);
+        exit(0);
+    }
+
+    std::string plannerName = "LAMA";
+    std::string domainFilename;
+    std::string problemFilename; 
+
+    if(firstArg == "-p" && argc == 5)
+    {
+        plannerName = argv[2];
+        domainFilename = argv[3];
+        problemFilename = argv[4];
+    } else if(argc == 3) {
+        domainFilename = argv[1];
+        problemFilename = argv[2];
+    } else {
+        usage(argc, argv);
+        exit(0);
+    }
+
+    std::map<std::string, PDDLPlannerInterface*>::iterator it = knownPlanners.find(plannerName);
+    if(it != knownPlanners.end())
+    {
+        planner = it->second;
+    } else {
+        printf("Could not find a planner: '%s'\n", plannerName.c_str());
+        exit(-1);
+    }
+
+    PDDLPlanner pddlPlanner(planner);
+    FILE* domainFile = fopen(domainFilename.c_str(),"r"); 
+    if(!domainFile)
+    {
+        printf("Error opening file: '%s' -- %s", domainFilename.c_str(), strerror(errno));
+        exit(-1);
+    }
+
+    char buffer[512];
+    std::string domainDescription;
+
+    while(fgets(buffer,512,domainFile) != NULL)
+    {
+        domainDescription += std::string(buffer);
+    }
+    fclose(domainFile);
+
+    pddlPlanner.setDomainDescription("test-domain", domainDescription);
+
+    FILE* problemFile = fopen(problemFilename.c_str(), "r");
+    if(!problemFile)
+    {
+        printf("Error opening file: '%s' -- %s", problemFilename.c_str(), strerror(errno));
+        exit(-1);
+    }
+
+    std::string problemDescription;
+    while(fgets(buffer, 512, problemFile) != NULL)
+    {
+        problemDescription += std::string(buffer);
+    }
+    fclose(problemFile);
 
 try {
-    PlanCandidates planCandidates = pddlPlanner.plan("(define (problem rimres-1)\n (:domain rimres)\n (:objects\n sherpa_0 crex_0 pl_0 - physob_id\n location_s0 location_c0 location_p0 - location\n mission1 - location\n)\n (:init \n (is_a sherpa_0 sherpa)\n (is_a crex_0 crex)\n (is_a pl_0 payload)\n (at sherpa_0 location_s0)\n (at crex_0 location_c0)\n (at pl_0 location_p0)\n (cannot_move pl_0)\n)\n (:goal (and \n (connected sherpa_0 crex_0) \n (connected sherpa_0 pl_0)\n (at sherpa_0 mission1)\n)\n)\n)\n");
-
+    PlanCandidates planCandidates = pddlPlanner.plan(problemDescription);
     printf("PlanCandidates:\n%s\n", planCandidates.toString().c_str());
 } catch(const std::runtime_error& e)
 {
