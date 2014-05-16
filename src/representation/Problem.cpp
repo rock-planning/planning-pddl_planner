547784db2d07f2cee24493c95ca506518b5d5590
Thomas Roehr <thomas.roehr@dfki.de>
2014-05-16 17:45:01 +0200

    Add problem representation

---
+++ src/representation/Problem.cpp
@@ -0,0 +1,84 @@
+#include "Problem.hpp"
+#include <sstream>
+#include <boost/foreach.hpp>
+
+namespace pddl_planner {
+namespace representation {
+
+Problem::Problem(const std::string& name, const Domain& domain)
+    : name(name)
+    , domain(domain)
+{}
+
+void Problem::addObject(const TypedItem& object, bool overwrite)
+{
+    objects.push_back( object );
+}
+
+void Problem::addInitialStatus(const Expression& e)
+{
+    if(!domain.isPredicate(e.label))
+    {
+        throw std::invalid_argument("pddl_planner::representation::Domain::addInitialStatus constant '" + e.label + "' already exists");
+    } else {
+        status.push_back(e);
+    }
+}
+
+void Problem::addGoal(const Expression& e)
+{
+    goals.push_back(e);
+}
+
+std::string Problem::toLISP() const
+{
+    std::stringstream ss;
+    ss << "; BEGIN problem definition" << std::endl;
+    ss << "(define (problem " << name << ")" << std::endl;
+    ss << "    (:domain " << domain.name << ")" << std::endl;
+    if(!objects.empty())
+    {
+        ss << "    (:objects " << std::endl;
+        BOOST_FOREACH(TypedItem t, objects)
+        {
+            ss << "        " << t.label << " - " << t.type << std::endl;
+        }
+        ss << "    )" << std::endl;
+    }
+
+    if(!status.empty())
+    {
+        ss << "    (:init " << std::endl;
+        BOOST_FOREACH(Expression e, status)
+        {
+            ss << "        " << e.toLISP() << std::endl;
+        }
+        ss << "    )" << std::endl;
+    }
+
+    if(!goals.empty())
+    {
+        ss << "    (:goal ";
+        if(goals.size() == 1)
+        {
+            ss << goals.front().toLISP() << std::endl;
+        } else {
+            ss << "(and";
+            BOOST_FOREACH(Expression e, goals)
+            {
+                ss << " " << e.toLISP();
+            }
+            ss << ")";
+        }
+        ss << ")" << std::endl;
+    }
+
+    ss << "; END problem definition" << std::endl;
+    ss << ")" << std::endl;
+
+    return ss.str();
+}
+
+
+} // end representation
+} // end pddl_planner
---
+++ src/representation/Problem.hpp
@@ -0,0 +1,52 @@
+#ifndef PDDL_PLANNER_REPRESENTATION_PROBLEM
+#define PDDL_PLANNER_REPRESENTATION_PROBLEM
+
+#include <pddl_planner/representation/Domain.hpp>
+
+namespace pddl_planner {
+namespace representation {
+
+struct Problem
+{
+    std::string name;
+    Domain domain;
+    ConstantList objects;
+
+    ExpressionList status;
+    ExpressionList goals;
+
+    Problem(const std::string& name, const Domain& domain);
+
+    /**
+     * Add additional constants -- these will be added to the domain definition
+     * which is associated with this problem
+     */
+    void addObject(const TypedItem& type, bool overwrite = false);
+
+    /**
+     * Add an expression defining the initial status
+     */
+    void addInitialStatus(const Expression& e);
+
+    /**
+     * Add a goal expression -- conjunction will be built for all added goals
+     */
+    void addGoal(const Expression& e);
+
+    /**
+     * Test if problem has been fully defined, i.e. that goals and a domain are set
+     * \return true if goals and a domain are provided, false otherwise
+     */
+    bool undefined() const { return goals.empty() || domain.isNull(); }
+
+    /**
+     * Generate the problem in a LISP representation
+     * \return problem definition in LISP
+     */
+    std::string toLISP() const;
+};
+
+} // end representation
+} // end pddl_planner
+
+#endif // PDDL_PLANNER_REPRESENTATION_PROBLEM
