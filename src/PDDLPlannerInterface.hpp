77a51d441ba883ff8b92bf93b2a3beae8585601c
Thomas Roehr <thomas.roehr@dfki.de>
2014-07-17 15:42:01 +0200

    Make PDDLPlannerInterface non abstract

--- src/PDDLPlannerInterface.hpp
+++ src/PDDLPlannerInterface.hpp
@@ -8,10 +8,27 @@ namespace pddl_planner
     class PDDLPlannerInterface
     {
     public:
-        virtual std::string getName() const = 0;
+        virtual ~PDDLPlannerInterface() {}
 
-        virtual int getVersion() const = 0;
+        /**
+         * Get name of this planner implementation
+         * \return Name of planner
+         * \throws PlanGenerationException if not implemented
+         */
+        virtual std::string getName() const { throw PlanGenerationException("PDDLPlannerInterface getName method not implemented"); }
 
+        /**
+         * Get version of this planner implementation
+         * \return version as int
+         * \throws PlanGenerationException if not implemented
+         */
+        virtual int getVersion() const { throw PlanGenerationException("PDDLPlannerInterface getVersion method not implemented"); }
+
+        /**
+         * Planning method that forwards problem, actions and domain to the underlying planning instance
+         * \return Solutions candidates
+         * \throws PlanGenerationException if not implemented
+         */
         virtual PlanCandidates plan(const std::string& problem, const std::string& actions, const std::string& domain) { throw PlanGenerationException("Plan method not implemented"); }
     };
 
