b5f2f8151e5538e74e96555293a869e83f456e1a
Thomas Roehr <thomas.roehr@dfki.de>
2014-07-23 17:59:11 +0200

    Add documentation

--- src/Planning.hpp
+++ src/Planning.hpp
@@ -131,7 +131,12 @@
    PlanCandidates planCandidates = planning.plan(problem);
  \endverbatim
  *
- *
+ * \section sec Weblinks and Resources
+ * Further information available at:
+ *     - http://ipc.informatik.uni-freiburg.de/PddlResources
+ *     - http://www.plg.inf.uc3m.es/ipc2011-deterministic/
+ *         - for a BNF description of PDDL 3.1
+ *             - http://www.plg.inf.uc3m.es/ipc2011-deterministic/attachments/Resources/kovacs-pddl-3.1-2011.pdf
  */
 
 namespace pddl_planner
